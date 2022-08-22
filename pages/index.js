d70a8613a696bda65741f1e28280365ef2fa5c00
hashibadaiki <higemeganeking@gmail.com>
2022-08-22 22:59:27 +0900

    スタイルまではざっくり完了した

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { jsonData } from "../src/assets/data";
 import { Container } from "../src/components/Container";
 import { Form } from "../src/components/Form";
 import { Spacer } from "../src/components/Spacer";
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
+	console.log(jsonData);
 	return (
 		<main>
 			<Container>
@@ -17,29 +20,53 @@ export default function Home() {
 					<a>ニュース一覧へ</a>
 				</Link>
 				<Spacer size={40} />
-				<section>
-					<h3>microCMS検索</h3>
-					<section>
-						<h4>本の検索</h4>
-						<Spacer size={16} />
-						<Form category="book" type="q" />
-						<Form category="book" type="contains" />
+				<div className={styles.wrapper}>
+					<section className={styles.microCMS}>
+						<h3>microCMS検索</h3>
+						<section>
+							<h4>本の検索</h4>
+							<Spacer size={16} />
+							<Form category="book" type="q" />
+							<Form category="book" type="contains" />
+						</section>
+						<Spacer size={24} />
+						<section>
+							<h4>ニュースの検索</h4>
+							<Spacer size={16} />
+							<Form category="news" type="q" />
+							<Form category="news" type="contains" />
+						</section>
+						<Spacer size={24} />
+						<section>
+							<h4>複合</h4>
+							<Spacer size={16} />
+							<Form category="" type="マルチ" />
+						</section>
 					</section>
-					<Spacer size={24} />
-					<section>
-						<h4>ニュースの検索</h4>
-						<Spacer size={16} />
-						<Form category="news" type="q" />
-						<Form category="news" type="contains" />
+					<Spacer size={8} />
+					<section className={styles.fusejs}>
+						<h3>fusejs検索</h3>
+						<section>
+							<h4>本の検索</h4>
+							<Spacer size={16} />
+							<Form category="book" type="q" />
+							<Form category="book" type="contains" />
+						</section>
+						<Spacer size={24} />
+						<section>
+							<h4>ニュースの検索</h4>
+							<Spacer size={16} />
+							<Form category="news" type="q" />
+							<Form category="news" type="contains" />
+						</section>
+						<Spacer size={24} />
+						<section>
+							<h4>複合</h4>
+							<Spacer size={16} />
+							<Form category="" type="マルチ" />
+						</section>
 					</section>
-					<Spacer size={24} />
-					<section>
-						<h4>複合</h4>
-						<Spacer size={16} />
-						<Form category="" type="マルチ" />
-					</section>
-				</section>
-				{/* TODO: 検索設置C fusejs */}
+				</div>
 			</Container>
 		</main>
 	);
