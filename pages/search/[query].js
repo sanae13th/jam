f761577aa5aae9618ab4d740fc8c7468765d6f38
hashibadaiki <higemeganeking@gmail.com>
2022-08-23 15:33:43 +0900

    todo: 入力 form

--- pages/search/[query].js
+++ pages/search/[query].js
@@ -1,32 +1,62 @@
 import Link from "next/link";
-import { fuse } from "../../src/assets/data";
+import { jsonData } from "../../src/assets/data";
 import { Container } from "../../src/components/Container";
 import { Spacer } from "../../src/components/Spacer";
 import styles from "../../styles/list.module.scss";
+import Fuse from "fuse.js";
 
-export default function Home({ data, searchQuery }) {
-	const result = fuse.search(searchQuery);
-	console.log(searchQuery, result, "hogeeeee");
+export default function Home({ dataRaw, searchQuery }) {
+	const data = dataRaw[0];
+	const dataLength = data?.matches.length || 0;
+	const dataList = () => {
+		if (!data) return { news: "", books: "" };
+		const bookArr = data.matches.filter(
+			(content) => content.key === "book.title"
+		);
+		const newsArr = data.matches.filter(
+			(content) => content.key === "news.title"
+		);
+		const news = newsArr.map((content) => data.item.news[content.refIndex]);
+		const books = bookArr.map((content) => data.item.book[content.refIndex]);
+		return { news, books };
+	};
+	const { news, books } = dataList();
 
-	// console.log(data);
 	return (
 		<Container>
-			<h2>fusejs検索結果</h2>
+			<h2>fusejs検索結果(本とnewsのtitle検索)</h2>
 			<div>
 				検索ワード: <span className={styles.word}>{searchQuery}</span>
 			</div>
 			<Spacer size={24} />
 			<h3>検索結果</h3>
-			{/* <div>検索結果は{data.length}件です</div>
+			<div>本とnewsの検索結果は{dataLength}件です</div>
+			<Spacer size={24} />
+			<h3>本の検索結果</h3>
+			<div>検索結果は{books.length}件です</div>
 			<ul className={styles.wrapper}>
-				{data.map((data) => (
-					<li key={data.id}>
-						<Link href={`/books/${data.id}`}>
-							<a>[title] {data.title}</a>
-						</Link>
-					</li>
-				))}
-			</ul> */}
+				{books &&
+					books.map((data) => (
+						<li key={data.id}>
+							<Link href={`/book/${data.id}`}>
+								<a>[book title] {data.title}</a>
+							</Link>
+						</li>
+					))}
+			</ul>
+			<Spacer size={24} />
+			<h3>newsの検索結果</h3>
+			<div>検索結果は{news.length}件です</div>
+			<ul className={styles.wrapper}>
+				{news &&
+					news.map((data) => (
+						<li key={data.id}>
+							<Link href={`/news/${data.id}`}>
+								<a>[news title] {data.title}</a>
+							</Link>
+						</li>
+					))}
+			</ul>
 			<Link href="/">
 				<a>TOPへ</a>
 			</Link>
@@ -43,11 +73,18 @@ export const getStaticPaths = async () => {
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async ({ params }) => {
-	const result = fuse.search("ho");
+	const options = {
+		threshold: 0,
+		includeMatches: true,
+		keys: ["book.title", "news.title"],
+	};
+
+	const fuse = new Fuse(jsonData, options);
+	const result = fuse.search(params.query);
 
 	return {
 		props: {
-			data: result,
+			dataRaw: result,
 			searchQuery: params.query,
 		},
 	};
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,12 +1,3 @@
 import data from "./data.json";
-import Fuse from "fuse.js";
 
-const jsonData = [data];
-
-const options = {
-	threshold: 0,
-	includeMatches: true,
-	keys: ["book.title", "news.title"],
-};
-
-export const fuse = new Fuse(jsonData, options);
+export const jsonData = [data];
