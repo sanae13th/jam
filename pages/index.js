ff0414d7b461cf29e173dd6de3f2b76ded9b70dc
hashibadaiki <higemeganeking@gmail.com>
2022-10-23 23:43:22 +0900

    不要なコードを全て削除した

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-import Link from "next/link";
 import { Container } from "../src/components/Container";
-import { Spacer } from "../src/components/Spacer";
 
 export default function Home() {
 	return (
 		<main>
-			<Container>
-				<h2>本の紹介サイト</h2>
-				<Spacer size={24} />
-				<Link href={`/book`}>
-					<a>本一覧へ</a>
-				</Link>
-			</Container>
+			<Container>Hello World!!</Container>
 		</main>
 	);
 }
